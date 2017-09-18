bb86dbb8c4019eae1ff66d1cccbade3c029b616a
velios <prostozapomnit@gmail.com>
2017-09-18 20:59:36 +0300

    Add some tests

--- tests.py
+++ tests.py
@@ -4,25 +4,39 @@ from format_price import format_price
 
 
 class FormatPriceTestCase(unittest.TestCase):
-    def setUp(self):
-        self.normal_test_value = '123456789.000'
-        self.error_test_value = 'qwerty'
-
     def test_is_result_string_type(self):
         self.assertIsInstance(
-            format_price(self.normal_test_value),
+            format_price('1234.1234'),
             str,
         )
 
     def test_is_correct_formatted(self):
         self.assertEqual(
-            format_price(self.normal_test_value),
-            '123 456 789.00',
+            format_price('123456789.12345'),
+            '123 456 789.12',
         )
 
     def test_is_correct_error_value_processing(self):
         self.assertIsNone(
-            format_price(self.error_test_value),
+            format_price('qwerty'),
+        )
+
+    def test_negative_number(self):
+        self.assertEqual(
+            format_price('-123456789.12'),
+            '-123 456 789.12'
+        )
+
+    def test_with_plus_number(self):
+        self.assertEqual(
+            format_price('+123456789.12'),
+            '123 456 789.12'
+        )
+
+    def test_with_comma_number(self):
+        self.assertEqual(
+            format_price('1234,1234'),
+            '1 234.12'
         )
 
 
